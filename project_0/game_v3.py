87d2bcd9ed21ca073331323e50d77e393557ff7e
royvudgren <gra-96@mail.ru>
2022-02-17 11:44:09 +0300

    new version of game (game_v3)

---
+++ project_0/game_v3.py
@@ -0,0 +1,61 @@
+"""Game "Guess a number 3.0" 
+(computer makes its own guess)"""
+
+import numpy as np
+
+def random_predicter(number:int=1) -> int:
+    """Random number guesser
+
+    Args:
+        number (int, optional): The number. Defaults to 1.
+
+    Returns:
+        int: Amount of attempts
+    """
+    count = 1
+    left_border = 1
+    right_border = 100
+    predict = round((left_border+right_border)/2)
+    
+    if not predict==number:
+        while True:
+            count += 1 
+            if number > predict:
+                left_border = predict
+                if left_border!=right_border-1:
+                    predict = round((left_border+right_border)/2)
+                else:
+                    predict = right_border
+            elif number < predict:
+                right_border = predict
+                if right_border!=left_border+1:
+                    predict = round((left_border+right_border)/2)
+                else:
+                    predict = left_border
+            elif number==predict:
+                break #cycle end after correct guess
+    return(count)
+
+def score_gameer(random_predicter) -> int:
+    """Average amount of attempts to guess a number in 1000 reps
+
+    Args:
+        random_predicter ([type]): guess function
+
+    Returns:
+        int: average amount of attempts
+    """
+    count_ls = []
+    np.random.seed(1) # fix the seed for reproducibility
+    random_array = np.random.randint(1,101, size=(1000)) # Made a list of numbers
+    
+    for number in random_array:
+        count_ls.append(random_predicter(number))
+        
+    score = int(np.mean(count_ls))
+    print(f"The algorithm guesses a number in an average of {score} attempts")
+    return(score)
+
+if __name__=='__main__':
+    # RUN
+    score_gameer(random_predicter)
